ab92487a978fec036a6361d186846e0757b4216e
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-11-14 08:46:54 +0700

    add redis

--- src/configs/index.js
+++ src/configs/index.js
@@ -2,5 +2,8 @@ require('dotenv').config()
 
 module.exports = {
     PORT: process.env.PORT,
-    IPBIND: process.env.IPBIND
+    IPBIND: process.env.IPBIND,
+    REDIS_PORT: process.env.REDIS_PORT,
+    REDIS_HOST: process.env.REDIS_HOST,
+    REDIS_PASS: process.env.REDIS_PASS
 }
