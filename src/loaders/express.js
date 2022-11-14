0b03879f79b7d0eb07a5fadbff0325828ecc09be
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-11-14 08:24:11 +0700

    add nodemon and tailwind

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.set('views', path.join(__dirname, '/../../views'))
 app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, '/../../public')))
 
 module.exports = app
