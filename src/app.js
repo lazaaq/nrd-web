f32bc3d1c168fd405ee486ce6725b555897528c5
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2022-11-19 07:35:51 +0700

    create links and endpoints

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@ const { PORT, IPBIND, API_HOST } = require('./configs')
 const app = require('./loaders/express')
 
 app.get('/', (req, res) => {
-    res.render('./html/index.ejs');
-});
+    res.render('./html/index.ejs')
+})
+
+app.get('/explore', (req, res) => {
+    res.send('explore')
+})
+
+app.get('/messages', (req, res) => {
+    res.send('messages')
+})
+
+app.get('/groups', (req, res) => {
+    res.send('groups')
+})
+
+app.get('/achievement', (req, res) => {
+    res.send('achievement')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('profile')
+})
 
 app.listen({
     port: PORT,
     host: IPBIND
-});
+})
